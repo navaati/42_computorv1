4747144dc944255171e302828fc699a35f0b5e77
Léo Gillot-Lamure <lgillot-@student.42.fr>
2015-07-14 00:16:05 +0200

    Show proper messages

--- computor.hs
+++ computor.hs
@@ -7,6 +7,7 @@ import Text.Parsec
 import Text.ParserCombinators.Parsec.Number
 import Control.Monad.Except
 import Control.Monad.Writer
+import Text.Printf
 import System.Environment
 import System.IO
 
@@ -115,12 +116,39 @@ solve (Poly c b a) = case b^2 - 4 * a * c of -- Classical complex solution of 2n
                               b' = b :+ 0
                               sqrt_discr = sqrt $ discr :+ 0
 
+
+displayReducedPoly :: ReducedPoly -> String
+displayReducedPoly (RP []) = "0"
+displayReducedPoly (RP coeffs) = intercalate " + " (zipWith (printf "%f * X^%u") coeffs [(0 :: Int)..])
+
+displayDegree :: Degree -> String
+displayDegree MinusInf = "negative infinity"
+displayDegree (Degree n) = show n
+
+displayComplex :: Complex Double -> String
+displayComplex (a :+ 0) = show a
+displayComplex (0 :+ b) = printf "%fi" b
+displayComplex (a :+ b) = printf "%f + %fi" a b
+
+displaySolution :: Solution -> String
+displaySolution (TwoRoots (x1 :+ 0) (x2 :+ 0)) = printf "Discriminant is strictly positive, the two solutions are:\n%f\n%f" x1 x2
+displaySolution (TwoRoots x1 x2) = printf "Discriminant is strictly negative, the two solutions are:\n%s\n%s" (displayComplex x1) (displayComplex x2)
+displaySolution (DoubleRoot x) = printf "Discriminant is null, the double solution is: %f" x
+displaySolution (OneRoot x) = printf "The solution is: %f" x
+displaySolution AllReals = "This equation holds true whatever X is"
+
+displayError :: ComputorError -> String
+displayError WrongNumberOfArgument = "Only one argument accepted"
+displayError (ParseError err) = show err
+displayError DegreeMoreThan2 = "This program can only solve polynoms of degree 2 or less"
+displayError NoSolution = "This equation holds false whatever X is"
+
 computor :: [String] -> ComputorM Solution
 computor args = do
   rp <- reduce . (uncurry $ zipWithDefault (-) 0) <$> readEquation args
-  tell [show rp]
+  tell [printf "Reduced form: %s = 0" $ displayReducedPoly rp]
   let deg = getDegree rp
-  tell [show deg]
+  tell [printf "Polynomial degree: %s" $ displayDegree deg]
   poly <- reducedToPoly rp
   solve poly
 
@@ -130,7 +158,7 @@ main = do
   let (res, messages) = runWriter . runExceptT $ computor args
   putStr $ unlines messages
   case res of
-   (Right solution) -> print solution
+   (Right solution) -> putStrLn $ displaySolution solution
    (Left err) -> do
      hPutStrLn stderr "Error:"
-     hPrint stderr err
+     hPutStrLn stderr (displayError err)
