acae9ed77a41cc697ab90fd3b7bf44f1f8d4c715
Léo Gillot-Lamure <lgillot-@student.42.fr>
2015-07-28 21:45:46 +0200

    Add a test that feeds back the solution(s) into the polynom.
    I don't include it in the test suite since I can't make it pass with a
    satisfying precison

--- tests/Computor/Tests/Math.hs
+++ tests/Computor/Tests/Math.hs
@@ -1,11 +1,15 @@
+{-# LANGUAGE FlexibleInstances #-}
+
 module Computor.Tests.Math where
 
 import Test.QuickCheck
 import Data.Complex
 import Control.Monad
+import Text.Printf
 
 import Computor.Math
 import Computor.Error
+import Computor.Displaying
 
 prop_real_sqrt_prec :: Double -> Property
 prop_real_sqrt_prec x = x > 0 ==> (myRealSqrt x)^2 - x < 0.00000000001
@@ -63,3 +67,33 @@ prop_arbitrary_reducedPoly (RP cs) = not (null cs) ==> last cs /= 0
 
 prop_reducedToPoly_degree_error :: ReducedPoly -> Property
 prop_reducedToPoly_degree_error rp@(RP cs) = length cs > 3 ==> reducedToPoly rp === Left DegreeMoreThan2
+
+instance Arbitrary Poly where
+  arbitrary = liftM3 Poly sometimesZero sometimesZero sometimesZero
+
+class SomewhatNull a where
+  sNull :: a -> Property
+
+threshold :: Double
+threshold = 0.0001
+
+instance SomewhatNull Double where
+  sNull a = let prop = abs a < threshold
+            in counterexample (printf "|%f| > %f" a threshold) prop
+
+instance SomewhatNull (Complex Double) where
+  sNull a = let prop = magnitude a < threshold
+            in counterexample (printf "|%s| = %f > %f" (displayComplex a) (magnitude a) threshold) prop
+
+prop_solve_equation_holds :: Poly -> Property
+prop_solve_equation_holds p@(Poly c b a) = case solve p of
+  Left _ -> property Discard
+  Right AllReals -> forAll arbitrary test
+  Right (OneRoot x) -> test x
+  Right (DoubleRoot x) -> test x
+  Right (TwoRoots x1 x2) -> testComplex x1 .&&. testComplex x2
+  where
+    test :: Double -> Property
+    test x = sNull (c + b * x + a * x * x)
+    testComplex :: Complex Double -> Property
+    testComplex x = sNull $ (c :+ 0) + (b :+ 0) * x + (a :+ 0) * x * x
